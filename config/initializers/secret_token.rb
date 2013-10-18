d1b6dcf790b96e6fa03b4f735d16bcd15099192c
Jonathan Hinkey <jonhinkey@gmail.com>
2013-10-18 19:36:52 +0000

    description of change

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Pintrest::Application.config.secret_key_base = '0414c1298806333e46afd68dbaf52b7f0333db01c83e5e4d196df56a8d295a47a3f4ca07f95af28215c5faa84d38f075528780b104c035d1251ec73ba889c69c'
+Pintrest::Application.config.secret_key_base = ENV['SECRET_TOKEN']
