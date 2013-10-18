b2e68334682e9405f61df362aff11159b9e62585
Jonathan Hinkey <jonhinkey@gmail.com>
2013-10-18 18:42:30 +0000

    Initial Commit BWS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintrest::Application.config.secret_key_base = '0414c1298806333e46afd68dbaf52b7f0333db01c83e5e4d196df56a8d295a47a3f4ca07f95af28215c5faa84d38f075528780b104c035d1251ec73ba889c69c'
